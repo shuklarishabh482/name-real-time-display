e06a729fb1e065fc1ee41f2f499f7f2315fb1170
jyotishwar-codersboutique <jyotishwar.codersboutique@gmail.com>
2025-06-10 18:27:25 +0530

    I make a user-details using react useState hooks that display and reupdate  on changing everything

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,43 @@
 import { useState } from 'react';
 import './App.css' ;
 function App(){
-let [name , setName]  = useState("");
-
-     let onChange = (eventObj)=>{
-      console.log(eventObj.target.value);
-      setName(()=> eventObj.target.value);
-
+let obj = {
+  name : "" ,
+  email : "" 
 }
-// let onChange = (eventObj)=>{
-//       console.log(eventObj.target.value);
-//       setName(()=> eventObj.target.value);
-// }
+let [userDetail , setUserDetail] = useState(obj);
+
+let onSubmitHandler = (e)=>{
+e.preventDefault();
+console.log(e);
+console.log(e.target);
+let formList = e.target ;
+let name = formList[0].value;
+let email  = formList[1].value;
+console.log(name,email);
+userDetail.name = name ;
+userDetail.email = email ;
+ userDetail = {...userDetail}
+setUserDetail(()=> userDetail)
+}
+console.log(userDetail);
+
 return (
 <>
-<form>
-<input id='inp' placeholder='Enter Your Name' onChange={onChange}></input>
-<button type='submit'>Submit Name</button>
+<form onSubmit={onSubmitHandler}>
+  <input type='text' placeholder='name'></input>
+  <br></br>
+  <input type='email' placeholder='email'></input>
+  <br></br>
+  <input type='password' placeholder='password'></input>
+  <br></br>
+  <button type='submit' onSubmit={onSubmitHandler}>Submit Details</button>
 </form>
-<div><h1>Name:{name}</h1></div>
-    </>
-  )
-}
+<h5>Name : {userDetail.name}</h5>
+<h5>Email : {userDetail.email}</h5>
 
+</>
+)
+
+}
 export default App
