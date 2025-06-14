e82d44ccde9a040ddb0d596289e5cfd5886b4d74
jyotishwar-codersboutique <jyotishwar.codersboutique@gmail.com>
2025-06-14 13:34:32 +0530

    create password-generaotr using useEffect ,useRef, useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,68 @@
-import { useState } from 'react';
+//useCallback,useEffect,useRef ;
+import {useState,useCallback, useEffect, useRef} from 'react';
 import './App.css';
-import Card from './components/Card';
 import "./index.css";
-function App() {
-let [totLikes , setTotLikes] = useState(0);
-let [cityLike , setCityLike] = useState({
-    bangloreLikes : 0 ,
-    puneLikes : 0 ,
-    hyderabadLikes : 0 ,
-});
+const App = ()=>{
 
-let [noneOf,setNoneOf] = useState(0); 
-
-let bangLike = ()=>{
-if (totLikes < 100){
-    totLikes =  cityLike.bangloreLikes  + cityLike.puneLikes + cityLike.hyderabadLikes + noneOf 
-    setTotLikes(prevTotLikes => prevTotLikes + 1);
-    cityLike.bangloreLikes = cityLike.bangloreLikes + 1 ;
-    setCityLike(prevCityLike => ({...prevCityLike}));
- }
-}
-let puneLike = ()=>{
-   if (totLikes <= 100){
-// totLikes =  cityLike.bangloreLikes  + cityLike.puneLikes + cityLike.hyderabadLikes + noneOf 
-      cityLike.puneLikes = cityLike.puneLikes + 1 ;
-      setCityLike(prevCityLike =>({...prevCityLike}));
-      totLikes =  cityLike.bangloreLikes  + cityLike.puneLikes + cityLike.hyderabadLikes + noneOf 
-      setTotLikes(prevTotLikes => prevTotLikes + 1)
- }
+let [length,setLength] = useState(10);
+let [number,setNumber]  = useState(false);
+let [char,setChar] = useState(false);
+let [password,setPassword] = useState("");
+const pwdGenerator = useCallback(()=>{
+let pass= "";
+let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" ;
+if (number) str += "0123456789";
+if (char) str += "!@#$%^&*_~(){}[]=+-" ;
+for (let i = 0 ; i < length; i++){
+let index  = Math.floor(Math.random() * str.length + 1);
+pass = pass + str.charAt(index);
 
 }
 
-let hydLike = ()=>{
-if (totLikes <= 100){
-// setCityLike(prevTotLikes => prevTotLikes + 1);
+setPassword(()=> pass);
+},[length,number,char,setPassword]);
+console.log(pwdGenerator);
 
-cityLike.hyderabadLikes = cityLike.hyderabadLikes + 1;
-setCityLike(prevCityLike =>({...prevCityLike}));
- totLikes =  cityLike.bangloreLikes  + cityLike.puneLikes + cityLike.hyderabadLikes + noneOf 
-setTotLikes(prevTotLikes => prevTotLikes + 1)
+useEffect(()=>{
+  pwdGenerator();
+},[length,number,char,pwdGenerator]);
 
- }
-}
+const pwdRef = useRef(null);
+const copyPassword =  useCallback(()=>{
+pwdRef.current?.select()
+pwdRef.current?.setSelectionRange(0,5);
+ window.navigator.clipboard.writeText(password);
+},[password])
 
-let noneOfThese = ()=>{
-    if (totLikes < 100){
-        totLikes =  cityLike.bangloreLikes  + cityLike.puneLikes + cityLike.hyderabadLikes + noneOf 
-        setTotLikes(prevTotLikes => prevTotLikes + 1)
-        setNoneOf(prevNoneof =>  prevNoneof + 1)
-    }
-}
-
-return (  
+return (
 <>
-<h1>Voting System : {totLikes}</h1>
+<h1 className='text-white text-4xl text-center'>Password Generator</h1>
 <br></br>
-<br/>
-<button onClick={bangLike}>Banglore Likes Percent :{cityLike.bangloreLikes}%</button>
-<br/>
-<br></br>
-<button onClick={puneLike}>Pune Likes Percent : {cityLike.puneLikes}%</button>
-<br/>
-<br></br>
-<button onClick={hydLike}>Hyderabad Likes Percent:{cityLike.hyderabadLikes}%</button>
-<br></br>
-<button onClick={noneOfThese}>None of These :{noneOf}%</button>
-
-    </>
+<div className='h-[100px] w-[500px] border border-red-500'>
+<div className='w-full'><input type='text' value={password} readOnly  ref={pwdRef}/>
+<button className='bg-green-500' onClick={copyPassword}>Copy</button>
+</div>
+<div> 
+<span>
+<input type="range" min={10} max={50} className="w-1/2 cursor-pointer" id='length' value={length}
+onChange={(event)=>{setLength(()=> event.target.value)}}
+/>
+{/* <input type='checkbox' id='number'></input> */}
+<label htmlfor="length">Length : {length}</label>
+</span>
+<span>
+<input type='checkbox' id='number' onChange={()=>{setNumber((prevVal)=> prevVal ? false : true)}}></input>
+<label htmmlfor="number">Number</label>
+</span>
+<span>
+<input type='checkbox' id='char' onChange={()=>{setChar((prevVal)=>prevVal ? false : true)}}></input>
+<label htmlFor='char'>Characters</label>
+</span>
+</div>
+</div>
+ </>
   );
 }
+
 
 export default App;
